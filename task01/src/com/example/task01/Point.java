ab65f26d1bef13845459a659ce878d7c8e74c4f6
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-09-10 12:23:23 +0500

    task01 completed

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,12 +1,24 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
-
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point() {}
+    void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+    double distance(Point point) {
+        return Math.sqrt(Math.pow((point.x - x), 2) + Math.pow((point.y - y), 2));
+    }
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
