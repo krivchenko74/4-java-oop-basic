3e372ed9733bed1cdf84b9309a11d1f5eb07ab0e
Krivchenko Vyacheslav <krivchenko.vi74@gmail.com>
2025-09-13 12:08:50 +0500

    task04 completed

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,11 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point p1 = new Point(0 ,0);
+        Point p2 = new Point(0, 5);
+        Line line = new Line(p1, p2);
+        Point p3 = new Point(1, 4);
+        System.out.println(line);
+        System.out.print(line.isCollinearLine(p3));
     }
 }
